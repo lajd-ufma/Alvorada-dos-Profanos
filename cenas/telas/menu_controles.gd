194d0c3aa1e09b8f52c85b57c142c9dc9be2fdb3
Flávio Guilherme Santana <guilher95161@gmail.com>
2026-02-22 14:43:30 -0300

    Criação tela de menu de controles

---
+++ cenas/telas/menu_controles.gd
@@ -0,0 +1,5 @@
+extends ColorRect
+
+
+func _on_voltar_btn_pressed() -> void:
+	get_tree().change_scene_to_file("res://cenas/telas/MenuPrincipal.tscn")
--- cenas/telas/menu_game_over.gd
+++ cenas/telas/menu_game_over.gd
@@ -5,4 +5,4 @@ func _on_reiniciar_btn_pressed():
 	get_tree().reload_current_scene()
 	queue_free()
 func _on_sair_btn_pressed():
-	get_tree().quit()
+	get_tree().change_scene_to_file("res://cenas/telas/MenuPrincipal.tscn")
